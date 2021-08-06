4834cd8c7534bde7d7dadca950a042de8ddc1e3f
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2021-08-06 06:50:59 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
 /*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+          nav =document.getElementById(navId);
+          
+    if (toggle && nav ) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu');
 
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    // Active link
+    navLink.forEach(elem => elem.classList.remove('active'));
+    this.classList.add('active');
+
+    //remove menu mobile
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+
+}
+
+navLink.forEach(ele => ele.addEventListener('click', linkAction, false));
